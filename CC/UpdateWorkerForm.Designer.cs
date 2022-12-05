ef4666a46dd3652468a1be04dc20908a8caa71e3
arsenyrogatov <arsenyrogatov@gmail.com>
2022-12-05 04:29:33 +0300

    Добавьте файлы проекта.

---
+++ CC/UpdateWorkerForm.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace CC
+{
+    partial class UpdateWorkerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateWorkerForm));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.updateWorker_button = new System.Windows.Forms.Button();
+            this.ewPas2_maskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.ewPas1_maskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.ewId_label = new System.Windows.Forms.Label();
+            this.ewPhone_maskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.ewMail_textBox = new System.Windows.Forms.TextBox();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.ewFio_textBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.updateWorker_button);
+            this.panel2.Controls.Add(this.ewPas2_maskedTextBox);
+            this.panel2.Controls.Add(this.ewPas1_maskedTextBox);
+            this.panel2.Controls.Add(this.ewId_label);
+            this.panel2.Controls.Add(this.ewPhone_maskedTextBox);
+            this.panel2.Controls.Add(this.ewMail_textBox);
+            this.panel2.Controls.Add(this.label18);
+            this.panel2.Controls.Add(this.label17);
+            this.panel2.Controls.Add(this.label12);
+            this.panel2.Controls.Add(this.label11);
+            this.panel2.Controls.Add(this.ewFio_textBox);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(38, 42);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(448, 270);
+            this.panel2.TabIndex = 2;
+            // 
+            // updateWorker_button
+            // 
+            this.updateWorker_button.AutoSize = true;
+            this.updateWorker_button.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.updateWorker_button.Location = new System.Drawing.Point(88, 215);
+            this.updateWorker_button.Name = "updateWorker_button";
+            this.updateWorker_button.Size = new System.Drawing.Size(288, 27);
+            this.updateWorker_button.TabIndex = 29;
+            this.updateWorker_button.Text = "Изменить данные о сотруднике";
+            this.updateWorker_button.UseVisualStyleBackColor = true;
+            this.updateWorker_button.Click += new System.EventHandler(this.updateWorker_button_Click);
+            // 
+            // ewPas2_maskedTextBox
+            // 
+            this.ewPas2_maskedTextBox.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ewPas2_maskedTextBox.Location = new System.Drawing.Point(173, 103);
+            this.ewPas2_maskedTextBox.Mask = "000000";
+            this.ewPas2_maskedTextBox.Name = "ewPas2_maskedTextBox";
+            this.ewPas2_maskedTextBox.Size = new System.Drawing.Size(263, 23);
+            this.ewPas2_maskedTextBox.TabIndex = 27;
+            // 
+            // ewPas1_maskedTextBox
+            // 
+            this.ewPas1_maskedTextBox.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ewPas1_maskedTextBox.Location = new System.Drawing.Point(173, 69);
+            this.ewPas1_maskedTextBox.Mask = "0000";
+            this.ewPas1_maskedTextBox.Name = "ewPas1_maskedTextBox";
+            this.ewPas1_maskedTextBox.Size = new System.Drawing.Size(263, 23);
+            this.ewPas1_maskedTextBox.TabIndex = 26;
+            // 
+            // ewId_label
+            // 
+            this.ewId_label.AutoSize = true;
+            this.ewId_label.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ewId_label.Location = new System.Drawing.Point(49, 10);
+            this.ewId_label.Name = "ewId_label";
+            this.ewId_label.Size = new System.Drawing.Size(54, 17);
+            this.ewId_label.TabIndex = 25;
+            this.ewId_label.Text = "label20";
+            // 
+            // ewPhone_maskedTextBox
+            // 
+            this.ewPhone_maskedTextBox.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ewPhone_maskedTextBox.Location = new System.Drawing.Point(173, 136);
+            this.ewPhone_maskedTextBox.Mask = "+0 (999) 000-00-00";
+            this.ewPhone_maskedTextBox.Name = "ewPhone_maskedTextBox";
+            this.ewPhone_maskedTextBox.Size = new System.Drawing.Size(132, 23);
+            this.ewPhone_maskedTextBox.TabIndex = 22;
+            this.ewPhone_maskedTextBox.Text = "79357636118";
+            // 
+            // ewMail_textBox
+            // 
+            this.ewMail_textBox.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ewMail_textBox.Location = new System.Drawing.Point(134, 175);
+            this.ewMail_textBox.Name = "ewMail_textBox";
+            this.ewMail_textBox.Size = new System.Drawing.Size(302, 23);
+            this.ewMail_textBox.TabIndex = 19;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label18.Location = new System.Drawing.Point(3, 181);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(54, 18);
+            this.label18.TabIndex = 18;
+            this.label18.Text = "Почта";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label17.Location = new System.Drawing.Point(3, 137);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(76, 18);
+            this.label17.TabIndex = 16;
+            this.label17.Text = "Телефон";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(3, 103);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(140, 18);
+            this.label12.TabIndex = 6;
+            this.label12.Text = "Номер паспорта";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(3, 72);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(137, 18);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "Серия паспорта";
+            // 
+            // ewFio_textBox
+            // 
+            this.ewFio_textBox.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ewFio_textBox.Location = new System.Drawing.Point(113, 36);
+            this.ewFio_textBox.Name = "ewFio_textBox";
+            this.ewFio_textBox.Size = new System.Drawing.Size(323, 23);
+            this.ewFio_textBox.TabIndex = 3;
+            this.ewFio_textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ewFio_textBox_KeyPress);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(3, 41);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(42, 18);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "ФИО";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Код";
+            // 
+            // UpdateWorkerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(526, 352);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UpdateWorkerForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Изменить пользователя";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button updateWorker_button;
+        private System.Windows.Forms.MaskedTextBox ewPas2_maskedTextBox;
+        private System.Windows.Forms.MaskedTextBox ewPas1_maskedTextBox;
+        private System.Windows.Forms.Label ewId_label;
+        private System.Windows.Forms.MaskedTextBox ewPhone_maskedTextBox;
+        private System.Windows.Forms.TextBox ewMail_textBox;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox ewFio_textBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label2;
+    }
+}
